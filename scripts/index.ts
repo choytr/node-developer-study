32c44c8d85f24cf677521ed0a58e8f5195fb8868
choytr <143041680+choytr@users.noreply.github.com>
2025-10-06 05:37:56 -1000

    Add input queries and sent emails and proper output of emails from each query

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,6 @@
 import { z } from "https://deno.land/x/zod@v3.20.0/mod.ts";
 import { promises as fs } from "node:fs";
+import path from "node:path";
 import Papa from "npm:papaparse";
 import ProgressBar from "https://deno.land/x/progress@v1.3.4/mod.ts";
 import { encodeHex } from "https://deno.land/std@0.207.0/encoding/hex.ts";
@@ -43,16 +44,25 @@ type ResponseType = {
   time: string;
 };
 
-const sentEmailsFileName = "data/sent_emails.txt";
 
-const sentEmails: string[] = await fs.readFile(sentEmailsFileName, "utf8")
-  .then((content: string) => content.split("\n"));
+const LINE_PATTERN = /\r?\n/;
+
+const IN_PATH = "data/in";
+const OUT_PATH = "data/out";
+
+const QUERIES_FILE_NAME = path.join(IN_PATH, "queries.txt");
+const SENT_EMAILS_FILE_NAME = path.join(IN_PATH, "sent_emails.txt");
+
+const PAGES_PER_QUERY = 40;
+
+const queries: string[] = await fs.readFile(QUERIES_FILE_NAME, "utf8")
+  .then((content: string) => content.split(LINE_PATTERN));
+
+const sentEmails: string[] = await fs.readFile(SENT_EMAILS_FILE_NAME, "utf8")
+  .then((content: string) => content.split(LINE_PATTERN));
 console.log(`${sentEmails.length} emails sent`);
 
-const sentEmailsSet = new Set(sentEmails);
-
-const requestAmount = 40;
-let completed = 0;
+const sentEmailsSet = new Set<string>(sentEmails);
 
 // https://stackoverflow.com/a/6234804/7589775
 function escapeHTML(unsafe: string) {
@@ -64,32 +74,29 @@ function escapeHTML(unsafe: string) {
     .replace(/'/g, "&#039;");
 }
 
-const progress = Deno.isatty(Deno.stdout.rid)
-  ? new ProgressBar({
-    title: "Package progress:",
-    total: requestAmount,
-  })
-  : undefined;
-
-function buildURL(index: number, max = 250) {
+function buildURL(index: number, query: string, max = 250) {
   if (max > 250) {
     throw new Error("Max > 250 - the registry can't handle more than this.");
   }
 
   // we can get a max of 250 at a time, sorting by popularity only, and using an empty search query (by abusing text filters and using a redundant boost-exact:false filter)
-  return `https://registry.npmjs.com/-/v1/search?size=${max}&popularity=1.0&quality=0.0&maintenance=0.0&text=js&from=${index}`;
+  return `https://registry.npmjs.com/-/v1/search?size=${max}&popularity=1.0&quality=0.0&maintenance=0.0&text=${query}&from=${index}`;
 }
 
-function pageURL(page: number) {
-  return buildURL(page * 250);
+function pageURL(page: number, query: string) {
+  return buildURL(page * 250, query);
 }
 
-async function getPage(page: number, retries = 2): Promise<Package[]> {
+function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function getPage(page: number, query: string, retries = 2): Promise<Package[]> {
   if (retries === 0) {
     throw new Error("retries over");
   }
   try {
-    const request = await fetch(pageURL(page));
+    const request = await fetch(pageURL(page, query));
     // Add a delay between each call
     await wait(800);
     const response: ResponseType = await request.json();
@@ -102,24 +109,35 @@ async function getPage(page: number, retries = 2): Promise<Package[]> {
     // If an error occurred, likely because too many requests too fast, wait a bit then continue
     await wait(5000);
     console.log("retry", err);
-    return getPage(page, retries - 1);
+    return getPage(page, query, retries - 1);
   }
 }
 
-function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const allPackagesSet = new Set<Package>();
 
-const packages: Package[] = [];
-for (let i = 0; i < requestAmount; i++) {
-  const fetchedPackages = await getPage(i);
-  completed++;
-  if (progress) {
-    progress.render(completed);
-  } else {
-    console.log(`Completed ${completed} of ${requestAmount} requests.`);
+async function getPackages(query: string) {
+  console.log(`Fetching packages for query "${query}"...`);
+  let completed = 0;
+
+  const progress = Deno.isatty(Deno.stdout.rid)
+    ? new ProgressBar({
+      title: "Package progress:",
+      total: PAGES_PER_QUERY,
+    })
+    : undefined;
+
+  const packages: Package[] = [];
+  for (let i = 0; i < PAGES_PER_QUERY; i++) {
+    const fetchedPackages = await getPage(i, query);
+    completed++;
+    if (progress) {
+      progress.render(completed);
+    } else {
+      console.log(`Completed ${completed} of ${PAGES_PER_QUERY} requests.`);
+    }
+    packages.push(...fetchedPackages);
   }
-  packages.push(...fetchedPackages);
+  return packages;
 }
 
 function getEmailsFromPackage(pkg: Package): string[] {
@@ -133,15 +151,26 @@ function getEmailsFromPackage(pkg: Package): string[] {
   return emails;
 }
 
-// Get the emails from each package
-// Filter out duplicates
-const newEmails: string[] = packages
-  .flatMap((pkg) => getEmailsFromPackage(pkg))
-  .filter((email, i, unfiltered) =>
-    (unfiltered.indexOf(email) === i) && (!sentEmailsSet.has(email))
-  );
+const newEmailsSet = new Set<string>();
+for (const query of queries) {
+  const packages = await getPackages(query);
+  for (const pkg of packages) allPackagesSet.add(pkg);
+  // Get the emails from each package
+  // Filter out duplicates
+  const newEmails: string[] = packages
+    .flatMap((pkg) => getEmailsFromPackage(pkg))
+    .filter((email, i, unfiltered) =>
+      (unfiltered.indexOf(email) === i) && (!sentEmailsSet.has(email)) && (!newEmailsSet.has(email))
+    );
 
-fs.writeFile("data/new_emails.txt", newEmails.join("\n"));
+  fs.writeFile(path.join(OUT_PATH, `${query}_new_emails.txt`), newEmails.join("\n"));
+  for (const email of newEmails) newEmailsSet.add(email);
+}
+
+console.log(
+  `Wrote ${newEmailsSet.size} new emails to ${queries.length} files in data/out`,
+  `pulled from ${allPackagesSet.size} unique packages.`,
+);
 
 // if (packages.length !== 10000) {
 // 	const remaining = 10000 - packages.length;
@@ -158,48 +187,48 @@ fs.writeFile("data/new_emails.txt", newEmails.join("\n"));
 //
 // 	console.log(`Fetched an extra ${objects.length} packages.`);
 // }
-
-const packagesString = new TextEncoder().encode(JSON.stringify(packages));
-await Deno.writeFile("./raw.json", packagesString);
-await Deno.writeTextFile(
-  "./raw.json.hash",
-  encodeHex(await crypto.subtle.digest("SHA-256", packagesString)),
-);
-
-function optionallyFormat(arg: string | undefined, label: string): string {
-  if (!arg) {
-    return "";
-  }
-
-  return ` ([${label}](${arg}))`;
-}
-
-const mdContent = `# Packages
-
-Ordered list of top 10000 NPM packages:
-
-${
-  packages.map((
-    { name, links: { npm, homepage, repository }, description, version },
-    i,
-  ) =>
-    `${i + 1}. [${name}](${npm})
-    - ${escapeHTML(description ?? "")}
-    - v${version} ${optionallyFormat(homepage, "homepage")}${
-      optionallyFormat(repository, "repository")
-    }`
-  ).join("\n")
-}
-`;
-
-await Deno.writeTextFile("./src/PACKAGES.md", mdContent);
-
-console.assert(
-  packages.length === 10000,
-  "Expected 10000 packages. Did the remainder function fail?",
-);
-
-console.log(
-  `Wrote ${packages.length} packages to ./raw.json and ./src/PACKAGES.md.`,
-  `with ${new Set(packages.map((pkg) => pkg.name)).size} unique packages.`,
-);
+//
+// const packagesString = new TextEncoder().encode(JSON.stringify(allPackages));
+// await Deno.writeFile("./raw.json", packagesString);
+// await Deno.writeTextFile(
+//   "./raw.json.hash",
+//   encodeHex(await crypto.subtle.digest("SHA-256", packagesString)),
+// );
+//
+// function optionallyFormat(arg: string | undefined, label: string): string {
+//   if (!arg) {
+//     return "";
+//   }
+//
+//   return ` ([${label}](${arg}))`;
+// }
+//
+// const mdContent = `# Packages
+//
+// Ordered list of top 10000 NPM packages:
+//
+// ${
+//   allPackages.map((
+//     { name, links: { npm, homepage, repository }, description, version },
+//     i,
+//   ) =>
+//     `${i + 1}. [${name}](${npm})
+//     - ${escapeHTML(description ?? "")}
+//     - v${version} ${optionallyFormat(homepage, "homepage")}${
+//       optionallyFormat(repository, "repository")
+//     }`
+//   ).join("\n")
+// }
+// `;
+//
+// await Deno.writeTextFile("./src/PACKAGES.md", mdContent);
+//
+// console.assert(
+//   allPackages.length === 10000,
+//   "Expected 10000 packages. Did the remainder function fail?",
+// );
+//
+// console.log(
+//   `Wrote ${allPackages.length} packages to ./raw.json and ./src/PACKAGES.md.`,
+//   `with ${new Set(allPackages.map((pkg) => pkg.name)).size} unique packages.`,
+// );
